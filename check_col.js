9dd72be6a41a396927e8ca1f859c7bb96d9828bc
User <ibaitelexfree@gmail.com>
2026-02-18 03:46:44 +0700

    reparando apk, no compila

--- check_col.js
+++ check_col.js
@@ -5,10 +5,9 @@ async function run() {
         'https://xbledhifomblirxurtyv.supabase.co',
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhibGVkaGlmb21ibGlyeHVydHl2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDYyMjE5NywiZXhwIjoyMDg2MTk4MTk3fQ.tynAhTsdBLSv_FI4CbGhWfHLjmfmsl8SJaeiTRDsd_A'
     );
-    const { data, error } = await supabase.from('reservas_alquiler').select('*').limit(1);
+    const { data, error } = await supabase.from('profiles').select('*').limit(1);
     if (data && data[0]) {
-        console.log('Keys:', Object.keys(data[0]).join(', '));
-        console.log('Has fecha_pago:', 'fecha_pago' in data[0]);
+        console.log('Profiles Columns:', Object.keys(data[0]).join(', '));
     } else {
         console.log('No data or error:', error);
     }
