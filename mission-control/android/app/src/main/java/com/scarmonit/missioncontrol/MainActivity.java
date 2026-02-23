3eda3eb662255702db3f6f286272a0b42a09bbe0
ibaitelexfree-creator <getxobelaeskola@gmail.com>
2026-02-23 20:38:34 +0700

    fix(mission-control): fix APK build error by removing redundant manual plugin registration

--- mission-control/android/app/src/main/java/com/scarmonit/missioncontrol/MainActivity.java
+++ mission-control/android/app/src/main/java/com/scarmonit/missioncontrol/MainActivity.java
@@ -1,16 +1,5 @@
 package com.scarmonit.missioncontrol;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.community.firebasecrashlytics.FirebaseCrashlytics;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // Explicitly register Firebase Crashlytics plugin to prevent potential
-        // initialization issues during startup, especially in test environments.
-        registerPlugin(FirebaseCrashlytics.class);
-
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
